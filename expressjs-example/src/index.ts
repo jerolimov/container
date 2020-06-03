5c4f7b49882aa05651eb949f31872588ef105095
Christoph Jerolimov <jerolimov+git+2020@gmail.com>
2020-06-03 02:53:05 +0200

    Add shutdown script

--- expressjs-example/src/index.ts
+++ expressjs-example/src/index.ts
@@ -8,6 +8,23 @@ app.get('/', (_, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App is listening on port ${port}!`);
+});
+
+const shutdown = () => {
+  console.log('Closing http server.');
+  server.close(() => {
+    console.log('Http server closed.');
+  });
+}
+
+process.on('SIGINT', function() {
+  console.log('SIGINT signal received.');
+  shutdown();
+});
+
+process.on('SIGTERM', () => {
+  console.log('SIGTERM signal received.');
+  shutdown();
 });
