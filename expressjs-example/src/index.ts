34429ce65fdb3fec6525f7d8663ba024d1d0c443
Christoph Jerolimov <jerolimov+git+2020@gmail.com>
2020-06-03 02:43:45 +0200

    Simplify TS example

--- expressjs-example/src/index.ts
+++ expressjs-example/src/index.ts
@@ -2,13 +2,12 @@ import express from 'express';
 
 const port = process.env.PORT || 3000;
 
-// Create a new express app instance
 const app = express();
 
-app.get('/', function (_, res) {
+app.get('/', (_, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, function () {
+app.listen(port, () => {
   console.log(`App is listening on port ${port}!`);
 });
