801e5f009d7500384803a52ad8a1870ae64f4ab5
Christoph Jerolimov <jerolimov+git+2020@gmail.com>
2020-06-15 19:16:42 +0200

    Add "env-example"

---
+++ env-example/src/app.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import morgan from 'morgan';
+
+const app = express();
+
+app.get('/favicon.ico', (_, res) => res.status(404).send());
+app.use(morgan(process.env.HTTP_LOG_FORMAT || 'dev'));
+
+app.get('/', (_, res) => {
+  const env: Record<string, string | undefined> = process.env;
+  const sortedKeys = Object.keys(env);
+  sortedKeys.sort();
+  const envAsString = sortedKeys.reduce(
+    (prev, curr) => prev + curr + '=' + env[curr] + '\n',
+    '',
+  );
+  res.type('text/plain').send(envAsString);
+});
+
+export default app;
